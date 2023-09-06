ed8e4ceb8b9ccbed4246a96fa59faca202115fa0
Dell-User <Dell-User@DESKTOP-HCCU5E8>
2023-09-06 04:17:56 +0200

    Add project files.

---
+++ WinFormsApp3/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        MsgBox("Hi Charlie")
+        MsgBox("How are you?")
+        MsgBox("Hey I am also bigger")
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        MsgBox("How old are you?")
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MessageBox.Show("This is another way of writting my message!")
+        MessageBox.Show("This is faster way of writting my message! ")
+    End Sub
+End Class
